482b7a558e62478cebcf70fc489ebd6fabf4588e
Haris Ibragic <ibragicharis01@gmail.com>
2024-05-07 17:19:02 +0200

    create AppContext

--- src/context.js
+++ src/context.js
@@ -1,25 +1,59 @@
-import React, { useState, useContext } from 'react';
+// import React, { useState, useContext } from 'react';
+
+// const AppContext = React.createContext();
+
+// const AppProvider = ({ children }) => {
+//   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+
+//   const openSidebar = () => {
+//     setIsSidebarOpen(true);
+//   };
+//   const closeSidebar = () => {
+//     setIsSidebarOpen(false);
+//   };
+
+//   const openModal = () => {
+//     setIsModalOpen(true);
+//   };
+
+//   const closeModal = () => {
+//     setIsModalOpen(false);
+//   };
+
+//   return (
+//     <AppContext.Provider
+//       value={{
+//         isSidebarOpen,
+//         isModalOpen,
+//         openModal,
+//         closeModal,
+//         openSidebar,
+//         closeSidebar,
+//       }}
+//     >
+//       {children}
+//     </AppContext.Provider>
+//   );
+// };
+
+// export const useGlobalContext = () => {
+//   return useContext(AppContext);
+// };
+
+// export { AppContext, AppProvider };
+
+// vezba
+
+import React from 'react';
+import { useContext } from 'react';
+import { useState } from 'react';
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openSidebar = () => {
-    setIsSidebarOpen(true);
-  };
-  const closeSidebar = () => {
-    setIsSidebarOpen(false);
-  };
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
 
   return (
     <AppContext.Provider
@@ -37,7 +71,7 @@ const AppProvider = ({ children }) => {
   );
 };
 
-export const useGlobalContext = () => {
+export default useGlobalContext = () => {
   return useContext(AppContext);
 };
 
