99afc2faeaea938c8e117d6c88d6456b4ad7e9ca
Haris Ibragic <ibragicharis01@gmail.com>
2024-05-07 18:17:38 +0200

    finish sideBar app

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -46,23 +46,94 @@
 
 // export default Sidebar;
 
+// import React from 'react';
+// import { useGlobalContext } from './context';
+// import logo from './logo.svg';
+// import { social, links } from './data';
+// import { FaTimes } from 'react-icons/fa';
+
+// const Sidebar = () => {
+//   const { isSidebarOpen, closeSideBar } = useGlobalContext();
+
+//   return (
+//     <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sideBar'}`}>
+//       <div className='sidebar-header'>
+//         <img src={logo} alt={logo} className='logo' />
+//         <button className='close-btn' onClick={closeSideBar}>
+//           <FaTimes />
+//         </button>
+//       </div>
+//       <ul className='links'>
+//         {links.map(link => {
+//           const { id, url, text, icon } = link;
+//           return (
+//             <li key={id}>
+//               <a href={url}>
+//                 {icon}
+//                 {text}
+//               </a>
+//             </li>
+//           );
+//         })}
+//       </ul>
+//       <ul className='social-icons'>
+//         {social.map(socialLink => {
+//           const { id, url, icon } = socialLink;
+//           return (
+//             <li key={id}>
+//               <a href={url}>{icon}</a>
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     </aside>
+//   );
+// };
+
+// export default Sidebar;
+
 import React from 'react';
-import { useGlobalContext } from './context';
 import logo from './logo.svg';
-import { social, links } from './data';
+import { useGlobalContext } from './context';
 import { FaTimes } from 'react-icons/fa';
+import { social, links } from './data';
 
 const Sidebar = () => {
-  const { isSideBarOpen, closeSideBar } = useGlobalContext();
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
 
   return (
-    <aside className={isSideBarOpen ? 'sidebar show-sidebar' : 'sideBar'}>
+    //                                     model     prikazi          sakri
+    <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
       <div className='sidebar-header'>
-        <img src={logo} alt={logo} className='logo' />
-        <button className='close-btn' onClick={closeSideBar}>
+        <img src={logo} alt='logo' className='logo' />
+        <button className='close-btn' onClick={closeSidebar}>
           <FaTimes />
         </button>
       </div>
+      <ul className='links'>
+        {links.map(link => {
+          const { id, url, text, icon } = link;
+
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className='social-icons'>
+        {social.map(link => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
     </aside>
   );
 };
