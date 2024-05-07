3cf02946a20b0783cdef5328bbf5a51afd92aaae
Haris Ibragic <ibragicharis01@gmail.com>
2024-05-07 17:22:53 +0200

    add openSideBar,closeSideBar,openModal,closeModal

--- src/context.js
+++ src/context.js
@@ -55,6 +55,21 @@ const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -71,7 +86,7 @@ const AppProvider = ({ children }) => {
   );
 };
 
-export default useGlobalContext = () => {
+export const useGlobalContext = () => {
   return useContext(AppContext);
 };
 
